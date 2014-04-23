ce5c7f2a6d8f94abb5ddd813df4b06ac1ce29532
Mark Norgren <mark@marknorgren.com>
2014-04-22 21:22:27 -0500

    initial commit

---
+++ newseum-pdf-downloader.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+"""
+    Newseum PDF Downloader
+    2014 Mark Norgren
+
+    Edit PAPERS list to add the paper for download
+    Get paper names at: http://www.newseum.org/todaysfrontpages/
+    Follow the 'Readable PDF' link
+"""
+import urllib2
+import cookielib
+import os
+import time
+import asyncore
+
+# Papers list for download
+PAPERS = ["MN_ST", "MN_PP", "ND_TF", "ND_GFH", "NY_NYT"]
+
+NEWSEUM_PDF_URL="http://webmedia.newseum.org/newseum-multimedia/dfp/pdf22/{0}.pdf"
+NEWSEUM_IMG="http://www.newseum.org"
+
+def fetchTodaysPDFFor(paper):
+    """
+    Fetch the PDF for a paper
+    """
+    print "Grabbing frontpage for %s" % (paper)
+    todaysDate = time.strftime("%Y.%m.%d")
+    fileName = "%s-%s.pdf" % (paper, todaysDate)
+
+    page = NEWSEUM_PDF_URL.format(paper)
+    print "page URL: %s" % (page)
+    f = urllib2.urlopen(page)
+    with open(fileName, 'wb') as file:
+        file.write(f.read())
+    f.close()
+    
+
+def main():
+    """
+    Main code goes here
+    """
+    for paper in PAPERS:
+        fetchTodaysPDFFor(paper)
+
+
+if __name__ == '__main__':
+    main()
+
+
